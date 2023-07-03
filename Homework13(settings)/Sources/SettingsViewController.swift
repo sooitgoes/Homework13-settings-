6a6d0affb48876ed1588a9fdd1a6c3de0b30d59a
Mikhail Laty <laty.mischa@gmail.com>
2023-07-03 15:32:51 +0300

    Add property dataSettings and register cells.

--- Homework13(settings)/Sources/SettingsViewController.swift
+++ Homework13(settings)/Sources/SettingsViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    private var dataSettings: [[Settings]]?
 
     // MARK: - UI Elements
     private lazy var tableView: UITableView = {
-        let table = UITableView()
+        let table = UITableView(frame: .zero, style: .insetGrouped)
+        table.register(DefaultTableViewCell.self, forCellReuseIdentifier: "default")
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: "title")
+        table.register(LabelTableViewCell.self, forCellReuseIdentifier: "label")
+        table.register(SwitchTableViewCell.self, forCellReuseIdentifier: "switch")
         table.translatesAutoresizingMaskIntoConstraints = false
         return table
     }()
