26eae4a463e5f1ba636b4520dd9f7380829d50fb
Mikhail Laty <laty.mischa@gmail.com>
2023-07-04 17:47:13 +0300

    Configure DetailViewController and configure his constraints.

--- Homework13(settings)/Sources/DetailViewController.swift
+++ Homework13(settings)/Sources/DetailViewController.swift
@@ -8,9 +8,22 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    var settings: Settings?
+    
     // MARK: - UI Elements
+    private lazy var name: UILabel = {
+        let name = UILabel()
+        name.font = .systemFont(ofSize: 25, weight: .bold)
+        name.numberOfLines = 2
+        name.translatesAutoresizingMaskIntoConstraints = false
+        return name
+    }()
 
+    private lazy var icon: UIImageView = {
+        let icon = UIImageView()
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        return icon
+    }()
 
     // MARK: - LifeCycle
     override func viewDidLoad() {
@@ -18,21 +31,30 @@ class DetailViewController: UIViewController {
         view.backgroundColor = .systemMint
         setupHierarchy()
         setupLayout()
+        fillData()
     }
 
     // MARK: - Setup
     private func setupHierarchy() {
+        view.addSubview(icon)
+        view.addSubview(name)
 
     }
 
     private func setupLayout() {
         NSLayoutConstraint.activate([
+            icon.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            icon.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            icon.heightAnchor.constraint(equalToConstant: 50),
+            icon.widthAnchor.constraint(equalToConstant: 50),
 
+            name.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            name.topAnchor.constraint(equalTo: icon.bottomAnchor, constant: 40)
         ])
     }
 
-
-
-
-
+    private func fillData() {
+        name.text = settings?.name
+        icon.image = settings?.icon
+    }
 }
