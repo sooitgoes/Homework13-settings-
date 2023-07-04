d1de0db0fa41caf7fe558defabfb7c6a9eacbf64
Mikhail Laty <laty.mischa@gmail.com>
2023-07-04 17:08:20 +0300

    Create DetailViewController and configure his.

---
+++ Homework13(settings)/Sources/DetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.swift
+//  Homework13(settings)
+//
+//  Created by Михаил Латий on 04.07.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    // MARK: - UI Elements
+
+
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+        setupHierarchy()
+        setupLayout()
+    }
+
+    // MARK: - Setup
+    private func setupHierarchy() {
+
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+
+        ])
+    }
+
+
+
+
+
+}
