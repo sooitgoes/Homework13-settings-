66ef13333dba2839d8777c904a67e7138b1396ca
Mikhail Laty <laty.mischa@gmail.com>
2023-07-05 20:50:02 +0300

    Shorten the code in label and switch cell's.

--- Homework13(settings)/Sources/LabelTableViewCell.swift
+++ Homework13(settings)/Sources/LabelTableViewCell.swift
@@ -7,24 +7,7 @@
 
 import UIKit
 
-class LabelTableViewCell: UITableViewCell {
-    var settings: Settings? {
-        didSet {
-            name.text = settings?.name
-            icon.image = settings?.icon
-            guard name.text != "Сотовая связь" && name.text != "Режим модема" else {
-                return imageContainer.backgroundColor = .systemGreen
-            }
-        }
-    }
-
-    // MARK: - UI Elements
-    private lazy var name: UILabel = {
-        let name = UILabel()
-        name.font = .systemFont(ofSize: 15, weight: .regular)
-        name.translatesAutoresizingMaskIntoConstraints = false
-        return name
-    }()
+class LabelTableViewCell: DefaultTableViewCell {
 
     private lazy var labelOff: UILabel = {
         let labelOff = UILabel()
@@ -34,65 +17,17 @@ class LabelTableViewCell: UITableViewCell {
         return labelOff
     }()
 
-    private lazy var icon: UIImageView = {
-        let icon = UIImageView()
-        icon.translatesAutoresizingMaskIntoConstraints = false
-        return icon
-    }()
-
-    private lazy var imageContainer: UIView = {
-        let view = UIView()
-        view.clipsToBounds = true
-        view.layer.cornerRadius = 7
-        view.backgroundColor = .systemBlue
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-
-    // MARK: - Initializers
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupHierarchy()
-        setupLayout()
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    // MARK: - Setup
-    private func setupHierarchy() {
-        imageContainer.addSubview(icon)
-        addSubview(imageContainer)
-        addSubview(name)
+    override func setupHierarchy() {
+        super.setupHierarchy()
         contentView.addSubview(labelOff)
     }
 
-    private func setupLayout() {
+    override func setupLayout() {
+        super.setupLayout()
         NSLayoutConstraint.activate([
-            icon.centerYAnchor.constraint(equalTo: imageContainer.centerYAnchor),
-            icon.centerXAnchor.constraint(equalTo: imageContainer.centerXAnchor),
-            icon.heightAnchor.constraint(equalToConstant: 15),
-            icon.widthAnchor.constraint(equalToConstant: 20),
-
-            imageContainer.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            imageContainer.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 20),
-            imageContainer.heightAnchor.constraint(equalToConstant: 30),
-            imageContainer.widthAnchor.constraint(equalToConstant: 30),
-
-            name.centerYAnchor.constraint(equalTo: imageContainer.centerYAnchor),
-            name.leftAnchor.constraint(equalTo: imageContainer.rightAnchor, constant: 15),
-
             labelOff.centerYAnchor.constraint(equalTo: imageContainer.centerYAnchor),
             labelOff.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -10)
         ])
-    }
-
-    // MARK: - Reuse
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        self.accessoryType = .none
-        self.settings = nil
     }
 }
 
--- Homework13(settings)/Sources/SwitchTableViewCell.swift
+++ Homework13(settings)/Sources/SwitchTableViewCell.swift
@@ -7,88 +7,25 @@
 
 import UIKit
 
-class SwitchTableViewCell: UITableViewCell {
-    var settings: Settings? {
-        didSet {
-            name.text = settings?.name
-            icon.image = settings?.icon
-            guard name.text != "VPN" else { return imageContainer.backgroundColor = .systemBlue}
-        }
-    }
-
-    // MARK: - UI Elements
-    private lazy var name: UILabel = {
-        let name = UILabel()
-        name.font = .systemFont(ofSize: 15, weight: .regular)
-        name.translatesAutoresizingMaskIntoConstraints = false
-        return name
-    }()
-
-    private lazy var icon: UIImageView = {
-        let icon = UIImageView()
-        icon.translatesAutoresizingMaskIntoConstraints = false
-        return icon
-    }()
-
-    private lazy var imageContainer: UIView = {
-        let view = UIView()
-        view.clipsToBounds = true
-        view.layer.cornerRadius = 7
-        view.backgroundColor = .orange
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-
+class SwitchTableViewCell: DefaultTableViewCell {
+    
     private lazy var switchControl: UISwitch = {
         let swich = UISwitch()
         swich.translatesAutoresizingMaskIntoConstraints = false
         return swich
     }()
-
-    // MARK: - Initializers
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupHierarchy()
-        setupLayout()
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    // MARK: - Setup
-    private func setupHierarchy() {
-        imageContainer.addSubview(icon)
-        addSubview(imageContainer)
-        addSubview(name)
+    
+    override func setupHierarchy() {
+        super.setupHierarchy()
         contentView.addSubview(switchControl)
     }
-
-    private func setupLayout() {
+    
+    override func setupLayout() {
+        super.setupLayout()
         NSLayoutConstraint.activate([
-            icon.centerYAnchor.constraint(equalTo: imageContainer.centerYAnchor),
-            icon.centerXAnchor.constraint(equalTo: imageContainer.centerXAnchor),
-            icon.heightAnchor.constraint(equalToConstant: 20),
-            icon.widthAnchor.constraint(equalToConstant: 20),
-
-            imageContainer.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            imageContainer.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 20),
-            imageContainer.heightAnchor.constraint(equalToConstant: 30),
-            imageContainer.widthAnchor.constraint(equalToConstant: 30),
-
-            name.centerYAnchor.constraint(equalTo: imageContainer.centerYAnchor),
-            name.leftAnchor.constraint(equalTo: imageContainer.rightAnchor, constant: 15),
-
             switchControl.centerYAnchor.constraint(equalTo: imageContainer.centerYAnchor),
             switchControl.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -20)
         ])
-    }
-
-    // MARK: - Reuse
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        self.accessoryType = .none
-        self.settings = nil
     }
 }
 
